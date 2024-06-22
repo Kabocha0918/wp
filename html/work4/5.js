c0695cd490dbae8470d7a74c30ab3869b30b6652
Kabocha0918 <161796716+Kabocha0918@users.noreply.github.com>
2024-06-22 15:11:33 +0800

    Create 5.js

---
+++ html/work4/5.js
@@ -0,0 +1,17 @@
+function countChar(str) {
+    let charCount = {};
+    
+    for (let char of str) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+    
+    return charCount;
+}
+
+console.log(countChar("aabccadeaac"));
+console.log(countChar("hello world"));
+
