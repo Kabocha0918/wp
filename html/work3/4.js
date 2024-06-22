efb7554107b4282bfa2bc48bc04cabc2a54ec0c4
Kabocha0918 <161796716+Kabocha0918@users.noreply.github.com>
2024-06-22 15:01:14 +0800

    Update 4.js

--- html/work3/4.js
+++ html/work3/4.js
@@ -20,6 +20,5 @@ function sumPrime(n) {
     return sum;
 }
 
-// 測試函數，假設 n 為 10
-console.log(sumPrime(10));  // 輸出: 17
-console.log(sumPrime(20));  // 輸出: 77
+console.log(sumPrime(10));  
+console.log(sumPrime(20));  
